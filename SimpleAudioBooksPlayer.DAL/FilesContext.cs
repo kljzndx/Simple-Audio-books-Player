8635d0f023ed20245050df1e602f3eb519bb456d
李继龙 <kljzndx@outlook.com>
2019-05-31 16:23:36 +0800

    完成DAL项目创建

---
+++ SimpleAudioBooksPlayer.DAL/FilesContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleAudioBooksPlayer.DAL
+{
+    public class FilesContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=files.db");
+        }
+    }
+}
---
+++ SimpleAudioBooksPlayer.DAL/ILibraryFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleAudioBooksPlayer.DAL
+{
+    public interface ILibraryFile
+    {
+        string FileName { get; set; }
+        string FilePath { get; set; }
+        DateTime ModifyTime { get; set; }
+        int DbVersion { get; set; }
+    }
+}
