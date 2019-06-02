e1a4e3bf2d6f085c845841627224b3a4e47a494e
李继龙 <kljzndx@outlook.com>
2019-06-02 15:03:39 +0800

    补充字段

--- SimpleAudioBooksPlayer.DAL/ILibraryFile.cs
+++ SimpleAudioBooksPlayer.DAL/ILibraryFile.cs
@@ -7,6 +7,10 @@ namespace SimpleAudioBooksPlayer.DAL
         string FileName { get; set; }
         string FilePath { get; set; }
         DateTime ModifyTime { get; set; }
+
+        string ParentFolderName { get; set; }
+        string ParentFolderPath { get; set; }
+
         int DbVersion { get; set; }
     }
 }
