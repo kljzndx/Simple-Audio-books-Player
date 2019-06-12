f30051bce6accacaccf0af4705b2b8e79cd653f0
李继龙 <kljzndx@outlook.com>
2019-06-12 08:34:07 +0800

    添加一些帮助方法和属性

--- SimpleAudioBooksPlayer/Models/DTO/MusicFileDTO.cs
+++ SimpleAudioBooksPlayer/Models/DTO/MusicFileDTO.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Email.DataProvider;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
+using GalaSoft.MvvmLight;
 using SimpleAudioBooksPlayer.DAL;
 
 namespace SimpleAudioBooksPlayer.Models.DTO
 {
-    public class MusicFileDTO
+    public class MusicFileDTO : ObservableObject
     {
+        private WeakReference<StorageFile> _file;
+        private MediaPlaybackItem _playbackItem;
+
+        private bool _isSelected;
+        private bool _isPlaying;
+
         public MusicFileDTO(MusicFile source)
         {
             GroupId = source.GroupId;
@@ -16,14 +27,61 @@ namespace SimpleAudioBooksPlayer.Models.DTO
             ModifyTime = source.ModifyTime;
         }
 
+        public MusicFileDTO(MusicFileDTO source)
+        {
+            _isPlaying = source.IsPlaying;
+
+            GroupId = source.GroupId;
+            Title = source.Title;
+            Duration = source.Duration;
+            FileName = source.FileName;
+            FilePath = source.FilePath;
+            ModifyTime = source.ModifyTime;
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => Set(ref _isSelected, value);
+        }
+
+        public bool IsPlaying
+        {
+            get => _isPlaying;
+            set => Set(ref _isPlaying, value);
+        }
+
         public int GroupId { get; }
 
-        public string Title { get; set; }
-        public TimeSpan Duration { get; set; }
+        public string Title { get; }
+        public TimeSpan Duration { get; }
 
-        public string FileName { get; set; }
-        public string FilePath { get; set; }
-        public DateTime ModifyTime { get; set; }
+        public string FileName { get; }
+        public string FilePath { get; }
+        public DateTime ModifyTime { get; }
 
+        private async Task<StorageFile> GetFile()
+        {
+            StorageFile file = null;
+            _file?.TryGetTarget(out file);
+
+            if (file is null)
+            {
+                file = await StorageFile.GetFileFromPathAsync(FilePath);
+                _file = new WeakReference<StorageFile>(file);
+            }
+
+            return file;
+        }
+
+        public async Task<MediaPlaybackItem> GetPlaybackItem()
+        {
+            var file = await GetFile();
+
+            if (_playbackItem is null)
+                _playbackItem = new MediaPlaybackItem(MediaSource.CreateFromStorageFile(file));
+
+            return _playbackItem;
+        }
     }
 }
