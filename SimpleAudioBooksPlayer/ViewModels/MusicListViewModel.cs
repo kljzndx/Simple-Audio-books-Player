1882e1f6cf97497d825969b9c7203cff609c678e
李继龙 <kljzndx@outlook.com>
2019-06-27 13:24:04 +0800

    加入排序相关方法

--- SimpleAudioBooksPlayer/ViewModels/MusicListViewModel.cs
+++ SimpleAudioBooksPlayer/ViewModels/MusicListViewModel.cs
@@ -1,62 +1,78 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.Linq;
-using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 using SimpleAudioBooksPlayer.Models.DTO;
+using SimpleAudioBooksPlayer.Models.Sorters;
 using SimpleAudioBooksPlayer.ViewModels.DataServer;
+using SimpleAudioBooksPlayer.ViewModels.SettingProperties;
 
 namespace SimpleAudioBooksPlayer.ViewModels
 {
     public class MusicListViewModel : ViewModelBase
     {
-        private MusicFileDataServer _server;
-        private ObservableCollection<MusicFileDTO> _data = new ObservableCollection<MusicFileDTO>();
+        private readonly MusicFileDataServer _server = MusicFileDataServer.Current;
+        private readonly MusicListSettingProperties _settings = MusicListSettingProperties.Current;
+
         private int _groupId;
+        private MusicListSortMembers _sortMethod;
 
         public MusicListViewModel()
         {
-            _server = MusicFileDataServer.Current;
-            _server.DataAdded += Server_DataAdded;
-            _server.DataRemoved += Server_DataRemoved;
+            SorterMembers.Add(new MusicSorterUi<MusicFileDTO>("TrackNumber", MusicSortDeserialization.Deserialize(MusicListSortMembers.TrackId)));
+            SorterMembers.Add(new MusicSorterUi<MusicFileDTO>("Title", MusicSortDeserialization.Deserialize(MusicListSortMembers.Name)));
+            SorterMembers.Add(new MusicSorterUi<MusicFileDTO>("ModifyDate", MusicSortDeserialization.Deserialize(MusicListSortMembers.ModifyTime)));
         }
 
-        public ObservableCollection<MusicFileDTO> Data
-        {
-            get => _data;
-            set => Set(ref _data, value);
-        }
+        public ObservableCollection<MusicFileDTO> Data { get; } = new ObservableCollection<MusicFileDTO>();
+        public List<MusicSorterUi<MusicFileDTO>> SorterMembers { get; } = new List<MusicSorterUi<MusicFileDTO>>();
 
         public void RefreshData(int groupId)
         {
-            foreach (var fileDto in _server.Data.Where(g => g.Group.Index == groupId))
-                Data.Add(fileDto);
+            if (groupId != _groupId)
+            {
+                _groupId = groupId;
+                _sortMethod = _settings.SortMethod;
 
-            _server.Data.CollectionChanged += Server_Data_CollectionChanged;
-            _groupId = groupId;
+                IEnumerable<MusicFileDTO> source = _server.Data.Where(g => g.Group.Index == groupId)
+                    .OrderBy(SorterMembers[(int) _sortMethod].KeySelector.Invoke);
+
+                if (_settings.IsReverse)
+                    source = source.Reverse();
+
+                var data = source.ToList();
+
+                Data.Clear();
+                foreach (var fileDto in data)
+                    Data.Add(fileDto);
+            }
+
+            if (_settings.SortMethod != _sortMethod)
+                SortData();
         }
 
-        private void Server_Data_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        private void SortData()
         {
-            if (e.NewItems != null)
-                foreach (MusicFileDTO item in e.NewItems)
-                    if (item.Group.Index == _groupId)
-                        Data.Add(item);
+            _sortMethod = _settings.SortMethod;
 
-            if (e.OldItems != null)
-                foreach (MusicFileDTO item in e.OldItems)
-                    if (Data.Contains(item))
-                        Data.Remove(item);
+            IEnumerable<MusicFileDTO> source = Data.OrderBy(SorterMembers[(int) _sortMethod].KeySelector.Invoke);
 
+            if (_settings.IsReverse)
+                source = source.Reverse();
+
+            var data = source.ToList();
+
+            for (var i = 0; i < data.Count; i++)
+                Data.Move(Data.IndexOf(data[i]), i);
         }
 
-        private void Server_DataAdded(object sender, IEnumerable<MusicFileDTO> e)
+        public void Reverse()
         {
-        }
+            _settings.IsReverse = !_settings.IsReverse;
 
-        private void Server_DataRemoved(object sender, IEnumerable<MusicFileDTO> e)
-        {
+            var data = Data.Reverse().ToList();
+            for (var i = 0; i < data.Count; i++)
+                Data.Move(Data.IndexOf(data[i]), i);
         }
     }
 }
