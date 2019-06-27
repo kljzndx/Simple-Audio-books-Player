08319be6db812c8c8808b5a1853d20b6ddf10cf1
李继龙 <kljzndx@outlook.com>
2019-06-27 15:17:47 +0800

    启用后退按钮

--- SimpleAudioBooksPlayer/Views/MainPage.xaml.cs
+++ SimpleAudioBooksPlayer/Views/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using Windows.System;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -15,15 +16,30 @@ namespace SimpleAudioBooksPlayer.Views
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SystemNavigationManager _navigationManager = SystemNavigationManager.GetForCurrentView();
+
         public MainPage()
         {
             this.InitializeComponent();
             CustomMediaPlayerElement.SetMediaPlayer(App.MediaPlayer);
+            _navigationManager.BackRequested += _navigationManager_BackRequested;
+        }
+
+        private void _navigationManager_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            Main_Frame.GoBack();
         }
 
         private async void Button_OnClick(object sender, RoutedEventArgs e)
         {
             await Launcher.LaunchFolderAsync(ApplicationData.Current.LocalFolder);
+        }
+
+        private void Main_Frame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            _navigationManager.AppViewBackButtonVisibility = Main_Frame.CanGoBack
+                ? AppViewBackButtonVisibility.Visible
+                : AppViewBackButtonVisibility.Collapsed;
         }
     }
 }
