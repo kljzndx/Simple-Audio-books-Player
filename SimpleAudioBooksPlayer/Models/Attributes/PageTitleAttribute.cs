349b0de8ba337f2ab1f2f04a480e5d65f405b751
李继龙 <kljzndx@outlook.com>
2019-07-09 12:16:26 +0800

    增加页面标题特性

---
+++ SimpleAudioBooksPlayer/Models/Attributes/PageTitleAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.ApplicationModel.Resources;
+
+namespace SimpleAudioBooksPlayer.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PageTitleAttribute : Attribute
+    {
+        public PageTitleAttribute(string resourceFile)
+        {
+            ResourceFile = resourceFile;
+        }
+
+        public PageTitleAttribute(string resourceFile, string resourceKey)
+        {
+            ResourceFile = resourceFile;
+            ResourceKey = resourceKey;
+        }
+
+        public string ResourceFile { get; set; }
+        public string ResourceKey { get; set; }
+
+        public string GetTitle()
+        {
+            return ResourceLoader.GetForCurrentView(ResourceFile).GetString(ResourceKey ?? "Title");
+        }
+    }
+}
--- SimpleAudioBooksPlayer/Views/MusicListPage.xaml.cs
+++ SimpleAudioBooksPlayer/Views/MusicListPage.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using SimpleAudioBooksPlayer.Models.Attributes;
 using SimpleAudioBooksPlayer.Models.DTO;
-using SimpleAudioBooksPlayer.Models.Sorters;
 using SimpleAudioBooksPlayer.ViewModels;
 using SimpleAudioBooksPlayer.ViewModels.DataServer;
 using SimpleAudioBooksPlayer.ViewModels.SettingProperties;
@@ -25,6 +13,7 @@ namespace SimpleAudioBooksPlayer.Views
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
+    [PageTitle("MusicListPage")]
     public sealed partial class MusicListPage : Page
     {
         private MusicListSettingProperties _settings = MusicListSettingProperties.Current;
