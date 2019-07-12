b1405fdd0d50c24d4e7afbe36ea8eaa04a019533
李继龙 <kljzndx@outlook.com>
2019-07-12 19:26:26 +0800

    为了能通过审核，再次修改排序算法

--- SimpleAudioBooksPlayer/Models/Sorters/SystemStringSorter.cs
+++ SimpleAudioBooksPlayer/Models/Sorters/SystemStringSorter.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace SimpleAudioBooksPlayer.Models.Sorters
 {
     public static class SystemStringSorter
     {
-        [DllImport("Shlwapi.dll", SetLastError = false, ExactSpelling = true, CharSet = CharSet.Unicode)]
-        private static extern int StrCmpLogicalW(string psz1, string psz2);
-
         public static int Compare(string x, string y)
         {
-            return StrCmpLogicalW(x, y);
+            return String.Compare(x, y, StringComparison.CurrentCulture);
         }
     }
 }
