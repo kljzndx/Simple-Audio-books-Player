da6a5c4499327792e0f7143296cb9ea5446ac2e6
李继龙 <kljzndx@outlook.com>
2019-07-15 08:54:57 +0800

    完成分类列表数据服务编写

--- SimpleAudioBooksPlayer.Service/ClassListDataService.cs
+++ SimpleAudioBooksPlayer.Service/ClassListDataService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using SimpleAudioBooksPlayer.DAL;
 
@@ -28,6 +29,50 @@ namespace SimpleAudioBooksPlayer.Service
             return _source;
         }
 
+        public async Task Rename(int index, string newName)
+        {
+            var item = _source.FirstOrDefault(c => c.Index == index);
+            if (item is null)
+                return;
 
+            item.Name = newName;
+            await _helper.Update(item);
+
+            DataUpdated?.Invoke(this, new[] {item});
+        }
+
+        public async Task SetBackgroundColor(int index, string newColor)
+        {
+            var item = _source.FirstOrDefault(c => c.Index == index);
+            if (item is null)
+                return;
+
+            item.BackgroundColor = newColor;
+            await _helper.Update(item);
+
+            DataUpdated?.Invoke(this, new[] {item});
+        }
+
+        public async Task Add(string name)
+        {
+            var item = new ClassItem(name);
+
+            await _helper.Add(item);
+            _source.Add(item);
+
+            DataAdded?.Invoke(this, new[] {item});
+        }
+
+        public async Task Remove(int index)
+        {
+            var item = _source.FirstOrDefault(c => c.Index == index);
+            if (item is null)
+                return;
+
+            await _helper.Remove(item);
+            _source.Remove(item);
+
+            DataRemoved?.Invoke(this, new[] {item});
+        }
     }
 }
