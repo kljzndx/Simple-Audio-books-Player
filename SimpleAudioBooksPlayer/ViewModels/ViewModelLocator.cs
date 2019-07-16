f20f87a642ea27f897e2734e6114484c807e9a1f
李继龙 <kljzndx@outlook.com>
2019-07-16 09:42:01 +0800

    完成分类列表框架编写

--- SimpleAudioBooksPlayer/ViewModels/ViewModelLocator.cs
+++ SimpleAudioBooksPlayer/ViewModels/ViewModelLocator.cs
@@ -12,14 +12,16 @@ namespace SimpleAudioBooksPlayer.ViewModels
 
             SimpleIoc.Default.Register<MainViewModel>();
             SimpleIoc.Default.Register<MusicListViewModel>();
+            SimpleIoc.Default.Register<ClassListViewModel>();
             SimpleIoc.Default.Register<GroupListViewModel>();
             SimpleIoc.Default.Register<PlayRecordViewModel>();
             SimpleIoc.Default.Register<SettingsViewModel>();
         }
 
         public MainViewModel Main => SimpleIoc.Default.GetInstance<MainViewModel>();
-        public MusicListViewModel MusicList => SimpleIoc.Default.GetInstance<MusicListViewModel>();
+        public ClassListViewModel ClassList => SimpleIoc.Default.GetInstance<ClassListViewModel>();
         public GroupListViewModel GroupList => SimpleIoc.Default.GetInstance<GroupListViewModel>();
+        public MusicListViewModel MusicList => SimpleIoc.Default.GetInstance<MusicListViewModel>();
         public PlayRecordViewModel PlayRecord => SimpleIoc.Default.GetInstance<PlayRecordViewModel>();
         public SettingsViewModel Settings => SimpleIoc.Default.GetInstance<SettingsViewModel>();
     }
