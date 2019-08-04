cf174bc96a3eb3873f09a5cd04b6b45734c131b0
李继龙 <kljzndx@outlook.com>
2019-08-04 17:14:47 +0800

    初步完成播放列表页面设计

---
+++ SimpleAudioBooksPlayer/ViewModels/SettingProperties/PlaybackViewSettingProperties.cs
@@ -0,0 +1,22 @@
+﻿using HappyStudio.UwpToolsLibrary.Auxiliarys;
+using HappyStudio.UwpToolsLibrary.Auxiliarys.Attributes;
+
+namespace SimpleAudioBooksPlayer.ViewModels.SettingProperties
+{
+    public class PlaybackViewSettingProperties : SettingsBase
+    {
+        public static readonly PlaybackViewSettingProperties Current = new PlaybackViewSettingProperties();
+
+        [SettingFieldByNormal(nameof(ListWidth), 240D)] private double _listWidth;
+
+        private PlaybackViewSettingProperties() : base("PlaybackView")
+        {
+        }
+
+        public double ListWidth
+        {
+            get => _listWidth;
+            set => SetSetting(ref _listWidth, value);
+        }
+    }
+}
---
+++ SimpleAudioBooksPlayer/Views/PlaybackListPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SimpleAudioBooksPlayer.ViewModels.SettingProperties;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace SimpleAudioBooksPlayer.Views
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class PlaybackListPage : Page
+    {
+        private static readonly CoreCursor NormalCursor = new CoreCursor(CoreCursorType.Arrow, 1);
+        private static readonly CoreCursor BothArrowCursor = new CoreCursor(CoreCursorType.SizeWestEast, 1);
+
+        private readonly PlaybackViewSettingProperties _settings = PlaybackViewSettingProperties.Current;
+
+        public PlaybackListPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Separator_Rectangle_OnManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            var newWidth = _settings.ListWidth + e.Delta.Translation.X;
+            if (newWidth > 220 && newWidth < ActualWidth - 400)
+                _settings.ListWidth = newWidth;
+        }
+
+        private void Separator_Rectangle_OnPointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = BothArrowCursor;
+        }
+
+        private void Separator_Rectangle_OnPointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = NormalCursor;
+        }
+    }
+}
