c10629389589de1a3f654ea24e34d6ba166e5219
李继龙 <kljzndx@outlook.com>
2019-08-04 10:03:45 +0800

    完成滚动字幕预览控件制作

---
+++ SimpleAudioBooksPlayer/ViewModels/SettingProperties/SubtitlePreviewSettingProperties.cs
@@ -0,0 +1,22 @@
+﻿using HappyStudio.UwpToolsLibrary.Auxiliarys;
+using HappyStudio.UwpToolsLibrary.Auxiliarys.Attributes;
+
+namespace SimpleAudioBooksPlayer.ViewModels.SettingProperties
+{
+    public class SubtitlePreviewSettingProperties : SettingsBase
+    {
+        public static readonly SubtitlePreviewSettingProperties Current = new SubtitlePreviewSettingProperties();
+
+        [SettingFieldByNormal(nameof(FontSize), 16D)] private double _fontSize;
+
+        private SubtitlePreviewSettingProperties() : base("SubtitlePreview")
+        {
+        }
+
+        public double FontSize
+        {
+            get => _fontSize;
+            set => SetSetting(ref _fontSize, value);
+        }
+    }
+}
---
+++ SimpleAudioBooksPlayer/Views/ItemTemplates/ScrollPreviewItemTemplate.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using HappyStudio.Subtitle.Control.UWP.Models;
+using SimpleAudioBooksPlayer.ViewModels.SettingProperties;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace SimpleAudioBooksPlayer.Views.ItemTemplates
+{
+    public sealed partial class ScrollPreviewItemTemplate : UserControl
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
+            nameof(Source), typeof(SubtitleLineUi), typeof(ScrollPreviewItemTemplate), new PropertyMetadata(null));
+
+        private readonly SubtitlePreviewSettingProperties _settings = SubtitlePreviewSettingProperties.Current;
+
+        private SubtitleLineUi _currentSource;
+
+        public event TypedEventHandler<ScrollPreviewItemTemplate, string> SelectionChanged;
+
+        public ScrollPreviewItemTemplate()
+        {
+            this.InitializeComponent();
+            Main_TextBlock.FontSize = _settings.FontSize;
+        }
+
+        public SubtitleLineUi Source
+        {
+            get => (SubtitleLineUi) GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+
+        private void ScrollPreviewItemTemplate_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContextChanged += ScrollPreviewItemTemplate_DataContextChanged;
+            _settings.PropertyChanged += Settings_PropertyChanged;
+
+            ScrollPreviewItemTemplate_DataContextChanged(null, null);
+        }
+
+        private void ScrollPreviewItemTemplate_OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (_currentSource != null)
+                _currentSource.PropertyChanged -= Source_PropertyChanged;
+
+            _settings.PropertyChanged -= Settings_PropertyChanged;
+            this.DataContextChanged -= ScrollPreviewItemTemplate_DataContextChanged;
+        }
+
+        private void ScrollPreviewItemTemplate_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            if (_currentSource != null)
+                _currentSource.PropertyChanged -= Source_PropertyChanged;
+
+            _currentSource = Source;
+
+            if (_currentSource != null)
+                _currentSource.PropertyChanged += Source_PropertyChanged;
+        }
+
+        private void Source_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(Source.IsSelected):
+                    if (Source.IsSelected)
+                    {
+                        Main_TextBlock.FontWeight = FontWeights.Bold;
+                        Main_TextBlock.FontSize = _settings.FontSize + 2;
+                    }
+                    else
+                    {
+                        Main_TextBlock.FontWeight = FontWeights.Normal;
+                        Main_TextBlock.FontSize = _settings.FontSize;
+                    }
+                    break;
+            }
+        }
+
+        private void Settings_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(_settings.FontSize):
+                    if (Source.IsSelected)
+                        Main_TextBlock.FontSize = _settings.FontSize + 2;
+                    else
+                        Main_TextBlock.FontSize = _settings.FontSize;
+                    
+                    break;
+            }
+        }
+
+        private void Main_TextBlock_OnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            SelectionChanged?.Invoke(this, Main_TextBlock.SelectedText);
+        }
+    }
+}
