00a2edef42fa4860443ee76177093732ec0048ff
李继龙 <kljzndx@outlook.com>
2019-08-06 12:23:12 +0800

    Fixed bugs

--- SimpleAudioBooksPlayer/ViewModels/DataServer/IFileDataServer.cs
+++ SimpleAudioBooksPlayer/ViewModels/DataServer/IFileDataServer.cs
@@ -1,6 +1,10 @@
-﻿namespace SimpleAudioBooksPlayer.ViewModels.DataServer
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleAudioBooksPlayer.ViewModels.DataServer
 {
     public interface IFileDataServer<T> : IDataServer<T, T>
     {
+        event EventHandler<IEnumerable<T>> DataUpdated;
     }
 }
