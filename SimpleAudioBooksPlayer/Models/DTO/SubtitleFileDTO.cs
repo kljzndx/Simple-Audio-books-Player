7d739c689d34cc9f3f12c36c4cc08403df30c2a9
李继龙 <kljzndx@outlook.com>
2019-08-07 22:16:10 +0800

    初步完成字幕解析方法编写

--- SimpleAudioBooksPlayer/Models/DTO/SubtitleFileDTO.cs
+++ SimpleAudioBooksPlayer/Models/DTO/SubtitleFileDTO.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using HappyStudio.Parsing.Subtitle;
+using HappyStudio.Subtitle.Control.UWP.Models;
+using HappyStudio.UwpToolsLibrary.Auxiliarys;
 using SimpleAudioBooksPlayer.DAL;
+using ObservableObject = GalaSoft.MvvmLight.ObservableObject;
 
 namespace SimpleAudioBooksPlayer.Models.DTO
 {
-    public class SubtitleFileDTO
+    public class SubtitleFileDTO : ObservableObject
     {
+        private WeakReference<List<SubtitleLineUi>> _linesReference;
+
         public SubtitleFileDTO(SubtitleFile source)
         {
             FileName = source.FileName;
@@ -16,6 +26,21 @@ namespace SimpleAudioBooksPlayer.Models.DTO
         public string FilePath { get; }
         public DateTime ModifyDate { get; private set; }
 
+        public async Task<List<SubtitleLineUi>> GetLines()
+        {
+            List<SubtitleLineUi> lines = null;
+            _linesReference?.TryGetTarget(out lines);
+            if (lines != null)
+                return lines;
+
+            var file = await StorageFile.GetFileFromPathAsync(FilePath);
+            var subtitle = SubtitleParser.Parse(await FileReader.ReadText(file, "GBK"));
+            lines = subtitle.Lines.Select(s => new SubtitleLineUi(s)).ToList();
+            _linesReference = new WeakReference<List<SubtitleLineUi>>(lines);
+
+            return lines;
+        }
+
         public void Update(SubtitleFile newSource)
         {
             if (newSource.FilePath != this.FilePath)
@@ -23,7 +48,7 @@ namespace SimpleAudioBooksPlayer.Models.DTO
 
             ModifyDate = newSource.ModifyTime;
 
-            // TODO 更新歌词列表
+            _linesReference = null;
         }
     }
 }
