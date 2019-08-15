6615ce4577565c9fc371b0e1f43fe4b171daca14
李继龙 <kljzndx@outlook.com>
2019-08-15 15:47:18 +0800

    新增几个设置项

--- SimpleAudioBooksPlayer/ViewModels/SettingProperties/SubtitlePreviewSettingProperties.cs
+++ SimpleAudioBooksPlayer/ViewModels/SettingProperties/SubtitlePreviewSettingProperties.cs
@@ -1,4 +1,5 @@
-﻿using HappyStudio.UwpToolsLibrary.Auxiliarys;
+﻿using System;
+using HappyStudio.UwpToolsLibrary.Auxiliarys;
 using HappyStudio.UwpToolsLibrary.Auxiliarys.Attributes;
 
 namespace SimpleAudioBooksPlayer.ViewModels.SettingProperties
@@ -8,6 +9,12 @@ namespace SimpleAudioBooksPlayer.ViewModels.SettingProperties
         public static readonly SubtitlePreviewSettingProperties Current = new SubtitlePreviewSettingProperties();
 
         [SettingFieldByNormal(nameof(FontSize), 16D)] private double _fontSize;
+        [SettingFieldByNormal(nameof(FontOpacity), 0.35D)] private double _fontOpacity;
+
+        [SettingFieldByNormal(nameof(SplitSymbols), "")] private string _splitSymbols;
+
+        [SettingFieldByNormal(nameof(IsRereadingModeEnable), false)] private bool _isRereadingModeEnable;
+        [SettingFieldByNormal(nameof(RereadingTimes), 1D)] private double _rereadingTimes;
 
         private SubtitlePreviewSettingProperties() : base("SubtitlePreview")
         {
@@ -19,12 +26,30 @@ namespace SimpleAudioBooksPlayer.ViewModels.SettingProperties
             set => SetSetting(ref _fontSize, value);
         }
 
-        [SettingFieldByNormal(nameof(FontOpacity), 0.4D)] private double _fontOpacity;
-
         public double FontOpacity
         {
             get => _fontOpacity;
             set => SetSetting(ref _fontOpacity, value);
+        }
+
+
+        public string SplitSymbols
+        {
+            get => _splitSymbols;
+            set => SetSetting(ref _splitSymbols, value);
+        }
+
+
+        public bool IsRereadingModeEnable
+        {
+            get => _isRereadingModeEnable;
+            set => SetSetting(ref _isRereadingModeEnable, value);
+        }
+
+        public double RereadingTimes
+        {
+            get => _rereadingTimes;
+            set => SetSetting(ref _rereadingTimes, value);
         }
     }
 }
