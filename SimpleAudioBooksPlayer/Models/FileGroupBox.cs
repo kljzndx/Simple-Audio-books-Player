b244277015ef585a94647240bed12d2acaeb8d4f
李继龙 <kljzndx@outlook.com>
2020-02-18 08:36:58 +0800

    完成FileGroupBox及其数据服务器编写

---
+++ SimpleAudioBooksPlayer/Models/FileGroupBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using SimpleAudioBooksPlayer.DAL;
+using SimpleAudioBooksPlayer.Models.DTO;
+
+namespace SimpleAudioBooksPlayer.Models
+{
+    /// <summary>
+    /// 书籍分组类
+    /// </summary>
+    public class FileGroupBox
+    {
+        public FileGroupBox(string path, IEnumerable<FileGroupDTO> list)
+        {
+            Name = path.TakeFolderName();
+            Path = path;
+            Groups = new ObservableCollection<FileGroupDTO>(list);
+            Groups.CollectionChanged -= Groups_CollectionChanged;
+            Groups.CollectionChanged += Groups_CollectionChanged;
+        }
+
+        public static event EventHandler HasBeenClear;
+
+        public string Name { get; }
+        public string Path { get; }
+        public ObservableCollection<FileGroupDTO> Groups { get; }
+
+        private void Groups_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (!Groups.Any())
+            {
+                HasBeenClear?.Invoke(this, EventArgs.Empty);
+                Groups.CollectionChanged -= Groups_CollectionChanged;
+            }
+        }
+    }
+}
---
+++ SimpleAudioBooksPlayer/ViewModels/DataServer/FileGroupBoxDataServer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using SimpleAudioBooksPlayer.DAL;
+using SimpleAudioBooksPlayer.Models;
+using SimpleAudioBooksPlayer.Models.DTO;
+
+namespace SimpleAudioBooksPlayer.ViewModels.DataServer
+{
+    public class FileGroupBoxDataServer : IDataServer<FileGroupBox, FileGroupBox>
+    {
+        public static readonly FileGroupBoxDataServer Current = new FileGroupBoxDataServer();
+
+        private readonly FileGroupDataServer _fileGroupDataServer = FileGroupDataServer.Current;
+
+        private FileGroupBoxDataServer()
+        {
+            Data = new ObservableCollection<FileGroupBox>();
+        }
+
+        public bool IsInit { get; private set; }
+        public ObservableCollection<FileGroupBox> Data { get; }
+
+        public event EventHandler<IEnumerable<FileGroupBox>> DataLoaded;
+        public event EventHandler<IEnumerable<FileGroupBox>> DataAdded;
+        public event EventHandler<IEnumerable<FileGroupBox>> DataRemoved;
+
+        public async Task Init()
+        {
+            if (_fileGroupDataServer.IsInit)
+                await _fileGroupDataServer.Init();
+
+            IsInit = true;
+            AutoGroup(_fileGroupDataServer.GetGroups(ClassListDataServer.Unspecified_ClassItem));
+
+            _fileGroupDataServer.DataAdded += FileGroupDataServer_DataAdded;
+            _fileGroupDataServer.DataRemoved += FileGroupDataServer_DataRemoved;
+            _fileGroupDataServer.ClassSeted += FileGroupDataServer_ClassSeted;
+            FileGroupBox.HasBeenClear += ClassGroup_HasBeenClear;
+
+            DataLoaded?.Invoke(this, Data.ToList());
+        }
+
+        private void AutoGroup(IList<FileGroupDTO> groups)
+        {
+            var cls = groups.GroupBy(g => g.FolderPath.TakeParentFolderPath());
+            bool hasAny = Data.Any();
+
+            List<FileGroupBox> additionList = new List<FileGroupBox>();
+
+            foreach (var cl in cls)
+            {
+                FileGroupBox classGroupItem = null;
+                if (hasAny)
+                    classGroupItem = Data.FirstOrDefault(cg => cg.Path == cl.Key);
+
+                if (classGroupItem == null)
+                {
+                    classGroupItem = new FileGroupBox(cl.Key, cl);
+                    Data.Add(classGroupItem);
+                    additionList.Add(classGroupItem);
+                }
+                else foreach (var groupDto in cl)
+                        classGroupItem.Groups.Add(groupDto);
+            }
+
+            if (additionList.Any())
+                DataAdded?.Invoke(this, additionList);
+        }
+
+        private void FileGroupDataServer_DataAdded(object sender, IEnumerable<FileGroupDTO> e)
+        {
+            AutoGroup(e.ToList());
+        }
+
+        private void FileGroupDataServer_DataRemoved(object sender, IEnumerable<FileGroupDTO> e)
+        {
+            var list = e.Where(fg => fg.ClassItem == ClassListDataServer.Unspecified_ClassItem);
+            foreach (var groupDto in list)
+                Data.FirstOrDefault(cg => cg.Groups.Contains(groupDto))?.Groups.Remove(groupDto);
+        }
+
+        private void FileGroupDataServer_ClassSeted(object sender, IEnumerable<FileGroupDTO> e)
+        {
+            var list = e.ToList();
+
+            if (list.FirstOrDefault()?.ClassItem == ClassListDataServer.Unspecified_ClassItem)
+                AutoGroup(list);
+            else foreach (var groupDto in list)
+                Data.FirstOrDefault(cg => cg.Groups.Contains(groupDto))?.Groups.Remove(groupDto);
+        }
+
+        private void ClassGroup_HasBeenClear(object sender, EventArgs e)
+        {
+            Data.Remove((FileGroupBox) sender);
+        }
+    }
+}
