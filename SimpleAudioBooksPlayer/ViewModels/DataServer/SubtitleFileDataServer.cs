e7a708c109c4b9492ce1d4be7ffd3da229096cb1
李继龙 <kljzndx@outlook.com>
2021-01-20 09:59:23 +0800

    增加异步锁

--- SimpleAudioBooksPlayer/ViewModels/DataServer/SubtitleFileDataServer.cs
+++ SimpleAudioBooksPlayer/ViewModels/DataServer/SubtitleFileDataServer.cs
@@ -7,6 +7,9 @@ using HappyStudio.UwpToolsLibrary.Auxiliarys.Files.Scanners;
 using SimpleAudioBooksPlayer.Models.DTO;
 using SimpleAudioBooksPlayer.Models.FileFactories;
 using SimpleAudioBooksPlayer.Models.FileModels;
+using Windows.ApplicationModel.Resources;
+using Nito.AsyncEx;
+using SimpleAudioBooksPlayer.ViewModels.Events;
 
 namespace SimpleAudioBooksPlayer.ViewModels.DataServer
 {
@@ -16,23 +19,33 @@ namespace SimpleAudioBooksPlayer.ViewModels.DataServer
 
         private FileScanner _scanner = new FileScanner("lrc", "srt");
         private SubtitleFileFactory _factory = new SubtitleFileFactory();
+        private readonly string _notificationString;
+        private readonly AsyncLock _mutex = new AsyncLock();
 
         public SubtitleFileDataServer()
         {
             _scanner.Options.FolderDepth = Windows.Storage.Search.FolderDepth.Shallow;
+            _notificationString = ResourceLoader.GetForCurrentView("Notifications").GetString("ScanningSubtitleFiles");
         }
 
         public ObservableCollection<SubtitleFile> Data { get; } = new ObservableCollection<SubtitleFile>();
 
         public async Task Scan(FileGroupDTO group)
         {
-            Data.Clear();
-
-            await _scanner.ScanByFileQuery(await StorageFolder.GetFolderFromPathAsync(group.FolderPath), async files =>
+            using (await _mutex.LockAsync())
             {
-                foreach (var file in files)
-                    Data.Add(await _factory.CreateByFile(file, group));
-            });
+                Data.Clear();
+
+                NotificationNotifier.RequestShow(_notificationString.Replace("$$", group.Name));
+
+                await _scanner.ScanByFileQuery(await StorageFolder.GetFolderFromPathAsync(group.FolderPath), async files =>
+                {
+                    foreach (var file in files)
+                        Data.Add(await _factory.CreateByFile(file, group));
+                });
+
+                NotificationNotifier.RequestHide();
+            }
         }
     }
 }
