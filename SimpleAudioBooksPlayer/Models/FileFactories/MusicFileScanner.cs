99ea9bf97906433f21a424493acaba5721e2d8b3
李继龙 <kljzndx@outlook.com>
2021-01-20 10:00:30 +0800

    增加扫描提示

--- SimpleAudioBooksPlayer/Models/FileFactories/MusicFileScanner.cs
+++ SimpleAudioBooksPlayer/Models/FileFactories/MusicFileScanner.cs
@@ -6,21 +6,27 @@ using HappyStudio.UwpToolsLibrary.Auxiliarys.Files.Scanners;
 using SimpleAudioBooksPlayer.Models.DTO;
 using SimpleAudioBooksPlayer.Models.FileModels;
 using Nito.AsyncEx;
+using SimpleAudioBooksPlayer.ViewModels.Events;
+using Windows.ApplicationModel.Resources;
 
 namespace SimpleAudioBooksPlayer.Models.FileFactories
 {
     public static class MusicFileScanner
     {
-        private static FileScanner _scanner = new FileScanner("mp3", "aac", "flac", "alac", "m4a", "wav");
+        public const string ExtensionName = "mp3 aac flac alac m4a wav";
+        
+        private static FileScanner _scanner = new FileScanner(ExtensionName.Split(' '));
         private static MusicFileFactory _factory = new MusicFileFactory();
 
         private static FileGroupDTO _groupDto;
         private static readonly List<MusicFile> _fileCaches = new List<MusicFile>();
         private static readonly AsyncLock _mutex = new AsyncLock();
+        private static readonly string _notificationString;
 
         static MusicFileScanner()
         {
             _scanner.Options.FolderDepth = Windows.Storage.Search.FolderDepth.Shallow;
+            _notificationString = ResourceLoader.GetForCurrentView("Notifications").GetString("ScanningMusicFiles");
         }
         
         public static async Task Scan(FileGroupDTO groupDTO, Func<IList<MusicFile>> data)
@@ -40,6 +46,8 @@ namespace SimpleAudioBooksPlayer.Models.FileFactories
                 _fileCaches.Clear();
                 _groupDto = groupDTO;
 
+                NotificationNotifier.RequestShow(_notificationString.Replace("$$", groupDTO.Name));
+
                 await _scanner.ScanByFileQuery(await StorageFolder.GetFolderFromPathAsync(groupDTO.FolderPath), async files =>
                 {
                     foreach (var file in files)
@@ -49,6 +57,8 @@ namespace SimpleAudioBooksPlayer.Models.FileFactories
                         _fileCaches.Add(mf);
                     }
                 });
+
+                NotificationNotifier.RequestHide();
             }
         }
     }
