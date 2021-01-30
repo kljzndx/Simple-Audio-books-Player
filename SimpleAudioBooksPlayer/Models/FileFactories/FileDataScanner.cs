774b022df06c25ca676bfd38f3ec48a938c199cf
李继龙 <kljzndx@outlook.com>
2021-01-30 12:32:09 +0800

    引入索引文件来加快扫描速度

--- SimpleAudioBooksPlayer/Models/FileFactories/FileDataScanner.cs
+++ SimpleAudioBooksPlayer/Models/FileFactories/FileDataScanner.cs
@@ -9,6 +9,9 @@ using Nito.AsyncEx;
 using SimpleAudioBooksPlayer.ViewModels.Events;
 using Windows.ApplicationModel.Resources;
 using System.Linq;
+using HappyStudio.UwpToolsLibrary.Auxiliarys.Files;
+using Newtonsoft.Json;
+using SimpleAudioBooksPlayer.Log;
 
 namespace SimpleAudioBooksPlayer.Models.FileFactories
 {
@@ -30,43 +33,70 @@ namespace SimpleAudioBooksPlayer.Models.FileFactories
             _subtitleNotificationString = ResourceLoader.GetForCurrentView("Notifications").GetString("ScanningSubtitleFiles");
         }
 
-        public static async Task<List<MusicFile>> ScanMusicData(FileGroupDTO groupDTO)
+        public static async Task<List<MusicFile>> ScanMusicData(FileGroupDTO groupDTO, bool isForceScan = false)
         {
             NotificationNotifier.RequestShow(_musicNotificationString.Replace("$$", groupDTO.Name));
 
-            var data = await Scan(groupDTO, new MusicFileFactory(), MusicFileExtensionNames);
+            var data = await Scan(groupDTO, new MusicFileFactory(), MusicFileExtensionNames, "music-files.json", isForceScan);
             
             NotificationNotifier.RequestHide();
             return data;
         }
 
-        public static async Task<List<SubtitleFile>> ScanSubtitleData(FileGroupDTO groupDTO)
+        public static async Task<List<SubtitleFile>> ScanSubtitleData(FileGroupDTO groupDTO, bool isForceScan = false)
         {
             NotificationNotifier.RequestShow(_subtitleNotificationString.Replace("$$", groupDTO.Name));
 
-            var data = await Scan(groupDTO, new SubtitleFileFactory(), SubtitleFileExtensionNames);
+            var data = await Scan(groupDTO, new SubtitleFileFactory(), SubtitleFileExtensionNames, "subtitle-files.json", isForceScan);
             
             NotificationNotifier.RequestHide();
             return data;
         }
 
-        private static async Task<List<TFile>> Scan<TFile>(FileGroupDTO groupDTO, IFileFactory<TFile> fileFactory, string extensionNames) where TFile: class, IFile
+        private static async Task<List<TFile>> Scan<TFile>(FileGroupDTO groupDTO, IFileFactory<TFile> fileFactory, string extensionNames, string cacheFileName, bool isForceScan) where TFile: LibraryFileBase
         {
             using (await _mutex.LockAsync())
             {
-                if (groupDTO.Equals(_groupDto))
+                if (!groupDTO.Equals(_groupDto))
                 {
-                    if (_fileCaches.ContainsKey(typeof(TFile)))
-                        return (List<TFile>)_fileCaches[typeof(TFile)];
-                }
-                else
-                {
+                    typeof(FileDataScanner).LogByType("清空缓存");
                     _groupDto = groupDTO;
                     _fileCaches.Clear();
                 }
 
                 var list = new List<TFile>();
-                
+
+                if (isForceScan)
+                {
+                    if (groupDTO.Equals(_groupDto) && _fileCaches.ContainsKey(typeof(TFile)))
+                    {
+                        typeof(FileDataScanner).LogByType("删除对应的缓存");
+                        _fileCaches.Remove(typeof(TFile));
+                    }
+                }
+                else
+                {
+                    if (groupDTO.Equals(_groupDto) && _fileCaches.ContainsKey(typeof(TFile)))
+                    {
+                        typeof(FileDataScanner).LogByType("直接返回缓存");
+                        return (List<TFile>)_fileCaches[typeof(TFile)];
+                    }
+
+                    typeof(FileDataScanner).LogByType("查找索引文件");
+                    if (await (await groupDTO.GetInfoFolder()).TryGetItemAsync(cacheFileName) is StorageFile cf)
+                    {
+                        typeof(FileDataScanner).LogByType("读取索引文件");
+                        var content = await FileReader.ReadText(cf, "GBK");
+                        list = JsonConvert.DeserializeObject<List<TFile>>(content);
+                        
+                        foreach (var file in list)
+                            file.Group = groupDTO;
+                        
+                        return list;
+                    }
+                }
+
+                typeof(FileDataScanner).LogByType("扫描文件");
                 var scanner = new FileScanner(extensionNames.Split(" "));
                 scanner.Options.FolderDepth = Windows.Storage.Search.FolderDepth.Shallow;
                 
@@ -78,6 +108,17 @@ namespace SimpleAudioBooksPlayer.Models.FileFactories
                         list.Add(data);
                     }
                 });
+
+                typeof(FileDataScanner).LogByType("添加缓存");
+                _fileCaches.Add(typeof(TFile), list);
+
+                if (list.Any())
+                {
+                    typeof(FileDataScanner).LogByType("写入索引文件");
+                    var infoFolder = await groupDTO.GetInfoFolder();
+                    var cacheFile = await infoFolder.CreateFileAsync(cacheFileName, CreationCollisionOption.OpenIfExists);
+                    await FileIO.WriteTextAsync(cacheFile, JsonConvert.SerializeObject(list));
+                }
 
                 return list;
             }
